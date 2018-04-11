44b2e06fc6d02e2a9ebeef727885a3d51f019a2f
Siddartha <sid.errabolu@gmail.com>
2018-04-10 20:32:58 -0400

    Iterable

--- core/src/main/java/com/bheaver/ngl4/importCatalog/impl/ImportMARCRecords.java
+++ core/src/main/java/com/bheaver/ngl4/importCatalog/impl/ImportMARCRecords.java
@@ -7,16 +7,21 @@ import com.bheaver.ngl4.importCatalog.util.Transformer;
 import org.marc4j.MarcReader;
 import org.marc4j.MarcStreamReader;
 import org.marc4j.marc.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ImportMARCRecords implements ImportCatalogRecords{
+    Logger logger = LoggerFactory.getLogger(ImportMARCRecords.class);
     @Autowired
     @Qualifier(BeanNames.TRANSFORMER)
     Transformer transformer;
@@ -28,22 +33,35 @@ public class ImportMARCRecords implements ImportCatalogRecords{
 
     @Override
     public Flux<MarcRecord> parseRecords(String rawData) {
-        return Flux.fromIterable(getMARCRecords(rawData)).subscribeOn(Schedulers.parallel());
+        return Flux.fromIterable(getMarcRecords(rawData)).subscribeOn(Schedulers.parallel());
+
+    }
+    private Iterable<MarcRecord> getMarcRecords(String rawData){
+        InputStream is = new ByteArrayInputStream(rawData.getBytes());
+        MarcReader reader = new MarcStreamReader(is);
+        return new RecordIterable(reader,transformer);
     }
 
-    private ArrayList<MarcRecord> getMARCRecords(String rawData){
-        ArrayList<MarcRecord> alReturn = new ArrayList<>();
-        try {
-            InputStream is = new ByteArrayInputStream(rawData.getBytes());
-            MarcReader reader = new MarcStreamReader(is);
-            while (reader.hasNext()) {
-                Record record = reader.next();
-                MarcRecord marcRecord = transformer.transform(record);
-                alReturn.add(marcRecord);
+}
+class RecordIterable implements Iterable<MarcRecord>{
+    private final MarcReader marcReader;
+    private final Transformer transformer;
+    public RecordIterable(MarcReader marcReader, Transformer transformer){
+        this.marcReader = marcReader;
+        this.transformer = transformer;
+    }
+    @Override
+    public Iterator<MarcRecord> iterator() {
+        return new Iterator<MarcRecord>() {
+            @Override
+            public boolean hasNext() {
+                return marcReader.hasNext();
             }
-        }catch (Exception exp){
-            exp.printStackTrace();
-        }
-        return alReturn;
+
+            @Override
+            public MarcRecord next() {
+                return transformer.transform(marcReader.next());
+            }
+        };
     }
 }
